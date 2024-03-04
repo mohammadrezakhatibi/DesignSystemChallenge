744e4b54a5355957b6dd5792fd29335da39c4dd7
Mohammadreza Khatibi <mohammadreza.khatibi@check24.de>
2024-03-04 12:09:19 +0100

    git init

---
+++ DesignSystemExample/DesignSystemExample/DesignSystemExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignSystemExampleApp.swift
+//  DesignSystemExample
+//
+//  Created by Mohammadreza Khatibi on 04.03.24.
+//
+
+import SwiftUI
+
+@main
+struct DesignSystemExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
